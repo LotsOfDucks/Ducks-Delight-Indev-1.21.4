a4ac32296f3a53f594cb3ec3a6ebdb51ae49e961
LotsOfDucks <113879225+LotsOfDucks@users.noreply.github.com>
2025-04-20 22:58:47 -0700

    Changed Rice Hunger Value

--- src/main/java/lod/ducksdelights/item/ModFoodComponents.java
+++ src/main/java/lod/ducksdelights/item/ModFoodComponents.java
@@ -4,8 +4,8 @@ import net.minecraft.component.type.FoodComponent;
 
 public class ModFoodComponents {
 
-    public static final FoodComponent WHITE_RICE = (new FoodComponent.Builder()).nutrition(1).saturationModifier(0.1F).build();
-    public static final FoodComponent GOLDEN_RICE = (new FoodComponent.Builder()).nutrition(2).saturationModifier(1.2F).build();
+    public static final FoodComponent WHITE_RICE = (new FoodComponent.Builder()).nutrition(2).saturationModifier(0.1F).build();
+    public static final FoodComponent GOLDEN_RICE = (new FoodComponent.Builder()).nutrition(4).saturationModifier(1.2F).build();
 
     private static FoodComponent.Builder createStew(int nutrition) {
         return (new FoodComponent.Builder()).nutrition(nutrition).saturationModifier(0.6F);

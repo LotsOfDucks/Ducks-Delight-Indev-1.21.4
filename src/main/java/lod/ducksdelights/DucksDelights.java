94cc091dce225e4671714bd8ef41904010f62aed
LotsOfDucks <113879225+LotsOfDucks@users.noreply.github.com>
2025-04-27 21:58:51 -0700

    Health Boost Potions

--- src/main/java/lod/ducksdelights/DucksDelights.java
+++ src/main/java/lod/ducksdelights/DucksDelights.java
@@ -6,16 +6,13 @@ import lod.ducksdelights.item.ModConsumableComponents;
 import lod.ducksdelights.item.ModFoodComponents;
 import lod.ducksdelights.item.ModItemGroups;
 import lod.ducksdelights.item.ModItems;
+import lod.ducksdelights.item.ModPotions;
 import lod.ducksdelights.sound.ModSounds;
 import net.fabricmc.api.ModInitializer;
 
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.SpawnGroup;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.registry.RegistryKey;
-import net.minecraft.registry.RegistryKeys;
-import net.minecraft.util.Identifier;
+import net.fabricmc.fabric.api.registry.FabricBrewingRecipeRegistryBuilder;
+import net.minecraft.item.Items;
+import net.minecraft.potion.Potions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -32,5 +29,12 @@ public class DucksDelights implements ModInitializer {
 		ModSounds.initialize();
 		ModFoodComponents.initialize();
 		ModConsumableComponents.initialize();
+		ModPotions.registerPotions();
+
+		FabricBrewingRecipeRegistryBuilder.BUILD.register(builder -> {
+			builder.registerPotionRecipe(Potions.AWKWARD, ModItems.MOTE_OF_CREATION, ModPotions.HEALTH_BOOST_POTION);
+			builder.registerPotionRecipe(ModPotions.HEALTH_BOOST_POTION, Items.REDSTONE, ModPotions.LONG_HEALTH_BOOST_POTION);
+			builder.registerPotionRecipe(ModPotions.HEALTH_BOOST_POTION, Items.GLOWSTONE_DUST, ModPotions.STRONG_HEALTH_BOOST_POTION);
+		});
 	}
 }
